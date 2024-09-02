e12cf92e2834ce6614083264815e279fb13d2f14
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-02 09:03:07 -0500

    Componente principal.

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"
 
 export const GifApp = () => {
+
+  const [categories, setcategories] = useState(['One Punch', 'Dragon Ball'])
+
+  const onAddCategory = () => {
+    setcategories([...categories, 'Valorant'])
+  }
+
   return (
     <div>
-      
+      <h1> Gif Browser </h1>
+      <button onClick={onAddCategory}> Agregar categoria</button>
+      <ol>
+        {categories.map(cat => (
+          <li> {cat} </li>
+        ))}
+      </ol>
     </div>
   )
 }
